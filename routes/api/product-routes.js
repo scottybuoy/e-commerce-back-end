ff6ce66a2e19f1cfbf0cd822bf85d0a627d8526d
Scott Everett <burningcupidxxx@gmail.com>
2022-02-07 23:07:53 -0800

    complete CRUD actions for product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll().then((productData) => {
+    res.json(productData);
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findByPk(req.params.id).then((productData) => {
+    res.json(productData);
+  })
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +97,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
 });
 
 module.exports = router;
