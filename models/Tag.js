9e714bed7d40e71ad69ea22b776bf6487c9d28d9
Scott Everett <burningcupidxxx@gmail.com>
2022-02-07 23:41:58 -0800

    define columns in ProductTag

--- models/Tag.js
+++ models/Tag.js
@@ -13,20 +13,23 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "product",
-        key: "id",
-      }
+    // product_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "product",
+    //     key: "id",
+    //   }
+    // },
+    tag_name: {
+      type: DataTypes.STRING
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "tag",
-        key: "id",
-      }
-    },
+    // tag_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "tag",
+    //     key: "id",
+    //   }
+    // },
   },
   {
     sequelize,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,55 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll().then((tagData) => {
+    res.json(tagData);
+  })
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findByPk(req.params.id).then((tagData) => {
+    res.json(tagData);
+  })
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((newTag) => {
+      res.json(newTag);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedTag) => {
+      res.json(deletedTag);
+    })
 });
 
 module.exports = router;
