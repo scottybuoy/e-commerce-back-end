083c3f1f206813a66128e692482d0b5a2e938582
Scott Everett <burningcupidxxx@gmail.com>
2022-02-07 22:34:24 -0800

    Add CRUD actions to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,19 +14,47 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findByPk(req.params.id).then((categoryData => {
+    res.json(categoryData);
+  }))
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory)
+    })
 });
 
 module.exports = router;
